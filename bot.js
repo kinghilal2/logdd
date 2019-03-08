dd3a13a389646d60b1970ef171101c01fc7080ec
kinghilal2 <47920420+kinghilal2@users.noreply.github.com>
2019-03-09 02:15:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2523,6 +2523,96 @@ client.on('message', message => {
 }
 });
  
+const credits = JSON.parse(fs.readFileSync("./creditsCode.json", "utf8"));
+const coolDown = new Set();
+
+client.on('message',async message => {
+    
+if(message.author.bot) return;
+if(!credits[message.author.id]) credits[message.author.id] = {
+    credits: 50
+};
+
+let userData = credits[message.author.id];
+let m = userData.credits;
+
+fs.writeFile("./creditsCode.json", JSON.stringify(credits), (err) => {
+    if (err) console.error(err);
+  });
+  credits[message.author.id] = {
+      credits: m + 0.5,
+  }
+  
+    if(message.content.startsWith(prefix + "credit" || prefix + "credits")) {
+message.channel.send(`**${message.author.username}, your :credit_card: balance is \`\`${userData.credits}\`\`.**`);
+}
+});
+
+client.on('message', async message => {
+    let amount = 250;
+    if(message.content.startsWith(prefix + "daily")) {
+    if(message.author.bot) return;
+    if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
+    
+    let userData = credits[message.author.id];
+    let m = userData.credits + amount;
+    credits[message.author.id] = {
+    credits: m
+    };
+
+    fs.writeFile("./creditsCode.json", JSON.stringify(userData.credits + amount), (err) => {
+    if (err) console.error(err);
+    });
+    
+    message.channel.send(`**:atm: | ${message.author.username}, you received your :yen: ${amount} credits!**`).then(() => {
+        coolDown.add(message.author.id);
+    });
+    
+    setTimeout(() => {
+       coolDown.remove(message.author.id);
+    },86400000);
+    }
+});
+
+client.on('message', async message => {
+    let amount = NaN;
+    if(message.content.startsWith(prefix + "1")) {
+    if(message.author.bot) return;
+    if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
+    
+    let userData = credits[message.author.id];
+    let m = userData.credits + amount;
+    credits[message.author.id] = {
+    credits: m
+    };
+    message.channel.send(`**:atm: | ${message.author.username}, you received your :yen: ${amount} credits!**`).then(() => {
+        coolDown.add(message.author.id);
+    });
+    
+ const credits = JSON.parse(fs.readFileSync("./creditsCode.json", "utf8")); const coolDown = new Set();
+	    client.on('message',async message => { if(message.author.bot) return;
+	if(!credits[message.author.id]) credits[message.author.id] = { credits: 50 };
+	let userData = credits[message.author.id];
+	let m = userData.credits;
+	fs.writeFile("./creditsCode.json", JSON.stringify(credits), (err) => { 
+		if (err) console.error(err); });credits[message.author.id] = { credits: m + 0.5, } 
+		if(message.content.startsWith(prefix + "credit" || prefix + "credits")) { message.channel.send(**${message.author.username}, your 💳 balance is \`${userData.credits}``.); } });
+	    client.on('message', async message => { let amount = 250;
+            if(message.content.startsWith(prefix + "daily")) { if(message.author.bot) return; 
+	    if(coolDown.has(message.author.id)) return message.channel.send(:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in ``1 Day``.);
+            let userData = credits[message.author.id]; 
+						  
+						  });
+	    client.on('message', async message => {
+         let m = userData.credits + amount; credits[message.author.id] = { credits: m };
+            fs.writeFile("./creditsCode.json", JSON.stringify(userData.credits + amount), (err) => { if (err) 
+	    console.error(err); }); message.channel.send(`**🏧 | ${message.author.username}, you received your 
+            💴 ${amount} credits!**`).then(() => { coolDown.add(message.author.id); }); setTimeout(() => { 		    
+            coolDown.remove(message.author.id); },86400000);
+		    
+	    };
+	    
+		    
 client.on('message', message => {
     if (message.content.startsWith("-avatar")) {
 if(!message.channel.guild) return;
@@ -2540,6 +2630,7 @@ if(!message.channel.guild) return;
     }
 });
 
+		    
 client.on('message', message => {
             var prefix = "-";
     if (message.author.bot) return;
